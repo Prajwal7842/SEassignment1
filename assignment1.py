9bbd9211f817c3a7a2ed101e4042eaa5c52c8f94
Prajwal7842 <prajwalgoel@gmail.com>
2020-08-18 09:36:41 +0530

    Add Division Code

--- assignment1.py
+++ assignment1.py
@@ -4,12 +4,17 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b;
 
+def division(a, b):
+	if b is not 0:
+		return a/b
+	return 0
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
+	print(division(a,b))
 
 
 '''
